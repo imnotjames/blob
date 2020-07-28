9e88fbcea91ec064aa823e16881fda749454de4f
James Ward <james@notjam.es>
2020-07-28 18:49:48 -0400

    use top level await instead of promise

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,15 @@
 import { getApp } from './app.js';
 import config from './config.js';
 
-getApp()
-  .then(
-    app => app.listen(
-      process.env.PORT || 8080,
-      () => process.stdout.write(`Listening on port ${config.port}\n`)
-    )
-  )
-  .catch(
-    (e) => {
-      process.stderr.write(e.stack);
+try {
+  const app = await getApp()
 
-      process.exit(1);
-    }
+  app.listen(
+    process.env.PORT || 8080,
+    () => process.stdout.write(`Listening on port ${config.port}\n`)
   );
+} catch (e) {
+  process.stderr.write(e.stack);
+
+  process.exit(1);
+}
